c4ee2dcceed310d27bd9b07e1075f85ed84400fc
amoghbl1 <amoghbl1@gmail.com>
2014-07-18 02:53:04 +0530

    added the required prefs to point browser to tor socks proxy.

--- mobile/android/base/GeckoApp.java
+++ mobile/android/base/GeckoApp.java
@@ -1100,6 +1100,9 @@ public abstract class GeckoApp
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+        //Setting all the required prefs.
+        makeMeAnon();
+        
         GeckoAppShell.registerGlobalExceptionHandler();
 
         // Enable Android Strict Mode for developers' local builds (the "default" channel).
@@ -1325,6 +1328,67 @@ public abstract class GeckoApp
         IntentHelper.init(this);
     }
 
+    private void makeMeAnon() {
+        
+        //Setting the user agent.
+        PrefsHelper.setPref("general.useragent.override", "Mozilla/5.0 (Windows NT 6.1; rv:17.0) Gecko/20100101 Firefox/17.0");
+        PrefsHelper.setPref("general.useragent.locale", "en-us,en;q=0.5");
+        
+        //Turn on proxying to local Tor / Orbot proxying by default
+
+        PrefsHelper.setPref("network.proxy.type",1); //manual proxy settings
+        PrefsHelper.setPref("network.proxy.http","localhost"); //manual proxy settings
+        PrefsHelper.setPref("network.proxy.http_port",8118); //manual proxy settings
+        PrefsHelper.setPref("network.proxy.socks","localhost"); //manual proxy settings
+        PrefsHelper.setPref("network.proxy.socks_port",9050); //manual proxy settings
+        PrefsHelper.setPref("network.proxy.socks_version",5); //manual proxy settings
+        
+        //Disable dish cacheing
+
+        PrefsHelper.setPref("browser.cache.disk.enable",false);
+        PrefsHelper.setPref("browser.cache.memory.enable",true); 
+        PrefsHelper.setPref("browser.cache.disk.capacity",0);
+        
+        //Ensure data is cleared on shutdown
+        PrefsHelper.setPref("privacy.clearOnShutdown.cache",true);
+        PrefsHelper.setPref("privacy.clearOnShutdown.cookies",true);
+        PrefsHelper.setPref("privacy.clearOnShutdown.downloads",true);
+        PrefsHelper.setPref("privacy.clearOnShutdown.formdata",true);
+        PrefsHelper.setPref("privacy.clearOnShutdown.history",true);
+        PrefsHelper.setPref("privacy.clearOnShutdown.offlineApps",true);
+        PrefsHelper.setPref("privacy.clearOnShutdown.passwords",true);
+        PrefsHelper.setPref("privacy.clearOnShutdown.sessions",true);
+        PrefsHelper.setPref("privacy.clearOnShutdown.siteSettings",true);
+        
+        //Do Not Track!
+        PrefsHelper.setPref("privacy.donottrackheader.enabled",false);
+        PrefsHelper.setPref("privacy.donottrackheader.value",1);
+        
+        //Disable 3rd party cookies
+        PrefsHelper.setPref("network.cookie.cookieBehavior", 1);
+
+        //Don't send a referrer
+        PrefsHelper.setPref("network.http.sendRefererHeader", 0);
+        
+        //Make sure certificates are up-to-date
+        PrefsHelper.setPref("security.OCSP.require", true); PrefsHelper.setPref("security.checkloaduri",true);
+
+        //Don't display mixed content (i.e. not secure content on a secure page)
+        PrefsHelper.setPref("security.mixed_content.block_display_content", true);
+
+        //Disable peer-to-peer WebRTC leak:
+        PrefsHelper.setPref("media.peerconnection.enabled",false); //webrtc disabled
+
+        //Disable ciphersuites that are not safe
+        //disable rc4
+        PrefsHelper.setPref("security.ssl3.ecdh_ecdsa_rc4_128_sha",false);
+        PrefsHelper.setPref("security.ssl3.ecdh_rsa_rc4_128_sha",false);
+        PrefsHelper.setPref("security.ssl3.ecdhe_ecdsa_rc4_128_sha",false);
+        PrefsHelper.setPref("security.ssl3.ecdhe_rsa_rc4_128_sha",false);
+        PrefsHelper.setPref("security.ssl3.rsa_rc4_128_md5",false);
+        PrefsHelper.setPref("security.ssl3.rsa_rc4_128_sha",false);
+    }
+    
     /**
      * At this point, the resource system and the rest of the browser are
      * aware of the locale.
